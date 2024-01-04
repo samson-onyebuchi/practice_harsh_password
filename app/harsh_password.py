cbf5ce7db35bd91fd195aa20c9be3305c1ea9e05
samson-onyebuchi <nnadisamson63@gmail.com>
2024-01-04 19:53:00 +0100

    still working on change password

--- app/harsh_password.py
+++ app/harsh_password.py
@@ -55,19 +55,27 @@ client = MongoClient(mongo_uri)
 db = client['TENCOWRY']  
 registered_emails_collection = db['TenCowry_eCommerce']
 
+# Route for changing the password
 @app.route('/change_password', methods=['POST'])
 def change_password():
+    # Extract parameters from raw JSON in the request body
     data = request.get_json()
 
     email = data.get('email')
     old_password_attempt = data.get('old_password')
     new_password = data.get('new_password')
 
+    # Find the user based on the provided email
     user = registered_emails_collection.find_one({"email": email})
 
-    if user and 'password_hash' in user and check_password_hash(user["password_hash"], old_password_attempt):
-        new_password_hash = generate_password_hash(f"a{new_password}z")
-        registered_emails_collection.update_one({"email": email}, {"$set": {"password_hash": new_password_hash}})
-        return "Password changed successfully."
-    else:
-        return "Invalid email or password.", 401
+    print("User document:", user)
+
+    if user and 'password_hash' in user:
+        if check_password_hash(user["password_hash"], old_password_attempt):
+            # If the user exists and the old password is correct, update the password hash
+            new_password_hash = generate_password_hash(f"a{new_password}z")
+            registered_emails_collection.update_one({"email": email}, {"$set": {"password_hash": new_password_hash}})
+            return "Password changed successfully."
+    
+    print("Login attempt failed.")
+    return "Invalid email or password.", 401
