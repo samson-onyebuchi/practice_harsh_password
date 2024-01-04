0e6d1b29f1abfa34967eb06169eba785c4904091
samson-onyebuchi <nnadisamson63@gmail.com>
2024-01-04 21:53:03 +0100

    API working perfectly for changepassword

--- app/harsh_password.py
+++ app/harsh_password.py
@@ -42,47 +42,52 @@
 # store_user_data("John", "Doe", "07038157217", "onyebuchisamson36@gmail.com", "")
 
 
-# from flask import Flask, request
-# from flask_bcrypt import generate_password_hash, check_password_hash
-# from pymongo import MongoClient
-# import os
+from flask import Flask, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from pymongo import MongoClient
+from flask_bcrypt import Bcrypt
+import os
 
-# app = Flask(__name__)
+app = Flask(__name__)
+bcrypt = Bcrypt()
 
-# # Connect to MongoDB Atlas (replace the placeholders with your actual connection string and database information)
-# mongo_uri = os.getenv("MONGO_URI") 
-# client = MongoClient(mongo_uri)
-# db = client['TENCOWRY']  
-# registered_emails_collection = db['TenCowry_eCommerce']
+# Connect to MongoDB Atlas (replace the placeholders with your actual connection string and database information)
+mongo_uri = os.getenv("MONGO_URI") 
+client = MongoClient(mongo_uri)
+db = client['TENCOWRY']  
+registered_emails_collection = db['TenCowry_eCommerce']
 
-# # Route for changing the password
-# @app.route('/change_password', methods=['POST'])
-# def change_password():
-#     # Extract parameters from raw JSON in the request body
-#     data = request.get_json()
+# Route for changing the password
+@app.route('/change_password', methods=['POST'])
+def change_password():
+    # Extract parameters from raw JSON in the request body
+    data = request.get_json()
 
-#     email = data.get('email')
-#     old_password_attempt = data.get('old_password')
-#     new_password = data.get('new_password')
+    email = data.get('email')
+    old_password_attempt = f"a{data.get('old_password')}z"
+    new_password = data.get('new_password')
 
-#     # Find the user based on the provided email
-#     user = registered_emails_collection.find_one({"email": email})
+    # Find the user based on the provided email
+    user = registered_emails_collection.find_one({"email": email})
 
-#     print("User document:", user)
+    print("User document:", user)
 
-#     if user and 'password_hash' in user:
-#         if check_password_hash(user["password_hash"], old_password_attempt):
-#             # If the user exists and the old password is correct, update the password hash
-#             new_password_hash = generate_password_hash(f"a{new_password}z")
-#             registered_emails_collection.update_one({"email": email}, {"$set": {"password_hash": new_password_hash}})
-#             return "Password changed successfully."
+    if user and 'password' in user:
+        if check_password_hash(user["password"], old_password_attempt):
+            # If the user exists and the old password is correct, update the password hash
+            new_password_hash = generate_password_hash(f"a{new_password}z")
+            registered_emails_collection.update_one({"email": email}, {"$set": {"password": new_password_hash}})
+            return "Password changed successfully."
     
-#     print("Login attempt failed.")
-#     return "Invalid email or password.", 401
+    print("Login attempt failed.")
+    return "Invalid email or password.", 401
 
 
 
-from werkzeug.security import generate_password_hash,check_password_hash
-#print(generate_password_hash("P1ain-text-user-passw@rd", "sha256"))
 
-print(check_password_hash("sha256$lTsEjTVv$c794661e2c734903267fbc39205e53eca607f9ca2f85812c95020f", "P1ain-text-user-passw@rd"))
+
+# from werkzeug.security import generate_password_hash,check_password_hash
+# #print(generate_password_hash("P1ain-text-user-passw@rd", "sha256"))
+
+# print(check_password_hash("sha256$lTsEjTVv$c794661e2c734903267fbc39205e53eca607f9ca2f85812c95020f", "P1ain-text-user-passw@rd"))
+
